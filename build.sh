dcf8466cb1e53a0f8f039063298fe5684b56042c
shwangyanfei <shwangyanfei@corp.netease.com>
2017-11-18 21:37:07 +0800

    re-orgnize code

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 
-./configure  --with-features=huge \
+ROOT_DIR=$(cd "$(dirname "$0")"; pwd)
+
+yum install -y ncurses-devel.x86_64
+yum install -y clang.x86_64
+yum install readline-devel.x86_64
+
+# lua 
+cd $ROOT_DIR/lua-5.3.4 && \
+  make linux && \
+  make install 
+
+
+# vim
+cd $ROOT_DIR/vim && \
+  ./configure  --with-features=huge \
   --enable-rubyinterp \
   --enable-pythoninterp \
   --enable-luainterp \
   --with-lua-prefix=/usr \
-  --prefix=/usr
+  --prefix=$HOME
 
